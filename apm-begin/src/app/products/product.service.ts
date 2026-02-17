fdc4c104ea196e8508364c82dc6504a86bd1d094
J. Junior <junior.jaava@gmail.com>
2026-02-17 09:24:43 +0100

    Add `getProduct` method to `ProductService` and integrate with `ProductDetailComponent` lifecycle hooks

--- apm-begin/src/app/products/product.service.ts
+++ apm-begin/src/app/products/product.service.ts
@@ -18,4 +18,12 @@ export class ProductService {
         tap(() => console.log('in http.get pipeline fetching products'))
       );
   }
+
+  getProduct(id: number): Observable<Product> {
+    const productUrl = `${this.productsUrl}/${id}`;
+    return this.http.get<Product>(productUrl)
+      .pipe(
+        tap(() => console.log(`in http.get pipeline fetching product ${id}`))
+      );
+  }
 }
