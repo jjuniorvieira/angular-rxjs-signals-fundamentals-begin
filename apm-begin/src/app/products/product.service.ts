7f7b25e078b6262218d104de03a817632adbd7a9
J. Junior <junior.jaava@gmail.com>
2026-02-20 19:36:29 +0100

    Handle errors in `getProducts` by adding `catchError` and fallback to mock data

--- apm-begin/src/app/products/product.service.ts
+++ apm-begin/src/app/products/product.service.ts
@@ -1,21 +1,26 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable, tap} from "rxjs";
+import {catchError, Observable, of, tap} from "rxjs";
 import {Product} from "./product";
+import {ProductData} from "./product-data";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  private productsUrl = 'api/products';
+  private productsUrl = 'api/productss';
 
   private http = inject(HttpClient);
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.productsUrl)
       .pipe(
-        tap(() => console.log('in http.get pipeline fetching products'))
+        tap(() => console.log('in http.get pipeline fetching products')),
+        catchError(err => {
+          console.error(err);
+          return of(ProductData.products)
+        })
       );
   }
 
