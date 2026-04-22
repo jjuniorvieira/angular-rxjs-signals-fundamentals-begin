ab52963d8d63babbdc9ee2ea4b5b2e4701dfda2f
J. Junior <junior.jaava@gmail.com>
2026-04-22 22:12:07 +0200

    replace map for switchMap

--- apm-begin/src/app/products/product.service.ts
+++ apm-begin/src/app/products/product.service.ts
@@ -1,6 +1,6 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient, HttpErrorResponse} from "@angular/common/http";
-import {catchError, map, Observable, of, tap, throwError} from "rxjs";
+import {catchError, map, Observable, of, switchMap, tap, throwError} from "rxjs";
 import {Product} from "./product";
 import {HttpErrorService} from "../utilities/http-error.service";
 import {ReviewService} from "../reviews/review.service";
@@ -30,7 +30,7 @@ export class ProductService {
     return this.http.get<Product>(productUrl)
       .pipe(
         tap(() => console.log(`in http.get pipeline fetching product ${id}`)),
-        map(product => this.getProductWithReviews(product)),
+        switchMap(product => this.getProductWithReviews(product)),
         catchError(err => this.handleError(err))
       );
   }
